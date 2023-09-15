7626d95fc339f1db1cea8104167370db2ea8818f
Joan Macharia <joan.macharia@students.moringa>
2023-09-15 23:02:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+    return cats
+}
+
+function appendCat(name){
+    const appendCat = [...cats, name]
+    return appendCat
+}
+
+function prependCat(name){
+    const prependCat = [name, ...cats]
+    return prependCat
+}
+
+function removeLastCat(){
+    const removeLastCat = cats.slice(0, cats.length-1)
+    return removeLastCat
+}
+
+function removeFirstCat(){
+    const removeFirstCat = cats.slice(1)
+    return removeFirstCat
+} 
